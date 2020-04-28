94add6fa466d7a0428cbc7aa649f3e3ef7b667da
khalil-asli <56821845+khalil-asli@users.noreply.github.com>
2020-04-28 12:39:27 +0200

    Update IService1.cs

--- TideTableWCF/IService1.cs
+++ TideTableWCF/IService1.cs
@@ -10,6 +10,7 @@ using TideTableWCF.Response;
 
 namespace TideTableWCF
 {
+    // using a ServiceContract to specify the operations that are available for the service client to consume. 
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
     public interface IService1
